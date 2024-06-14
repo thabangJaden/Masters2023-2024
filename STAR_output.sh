07f51a16f031e75f926044e2e8e37e96756e2cdd
thabangJaden <thabangjaden0@gmail.com>
2024-06-14 11:59:50 +0200

    Update STAR_output.sh

--- STAR_output.sh
+++ STAR_output.sh
@@ -6,10 +6,10 @@ tail -n +5 sample_output.ReadsPerGene.ou.tab | cut -f2 > sample_output_tmp.txt
 
 tail -n +5 sample_output.ReadsPerGene.ou.tab | cut -f1 > Gene_ID_tmp.txt
 
-#[2] Insert headers to the tmp files
+# [2] Insert headers to the tmp files
 echo -n -e "sample_header\n$(cat sample_output_tmp.txt )" > sample_output_tmp.txt 
 
-#{3} combines the sample    files into a table
+# [3] combines the sample files into a table
 paste *_tmp.txt > total_counts.tsv
 
 rm *_tmp.txt
